d276e30114a57d9297e8317511cd10b6f3add4fc
Nikos Theodosis <n.theodosis@stoiximan.gr>
2020-04-18 14:15:16 +0300

    add config items

--- lib/fastlane/plugin/discord_notifier/actions/discord_notifier_action.rb
+++ lib/fastlane/plugin/discord_notifier/actions/discord_notifier_action.rb
@@ -6,14 +6,31 @@ module Fastlane
   module Actions
     class DiscordNotifierAction < Action
       def self.run(params)
+        
         UI.message("Notifying Discord")
 
+        color = params[:success] ? "4BB543" : "CC0000"
 
+        unless params[:color].nil? || params[:color] == 0
+          color = params[:color]
+        end
+       
         client = Discordrb::Webhooks::Client.new(url: params[:webhook_url])
         client.execute do |builder|
           builder.add_embed do |embed|
-            embed.title = "Title"
-            embed.description = "Description"
+            embed.title = params[:title]
+            embed.description = params[:description]
+            embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(
+              url: params[:thumbnail_url]
+            )
+            embed.image = Discordrb::Webhooks::EmbedImage.new(
+              url: params[:image_url]
+            )
+            embed.author = Discordrb::Webhooks::EmbedAuthor.new(
+              name: params[:author]
+            )
+            embed.colour = color
+            embed.timestamp = Time.now
           end
         end
       end
@@ -42,6 +59,42 @@ module Fastlane
             env_name: "DISCORD_WEBHOOK_URL",
             description: "Discord Webhook URL",
             optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :title,
+            optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :description,
+            optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :thumbnail_url,
+            optional: true,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :image_url,
+            optional: true,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :author,
+            optional: true,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :success,
+            optional: true,
+            default_value: true,
+            is_string: false
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :color,
+            optional: true,
             type: String
           )
         ]
